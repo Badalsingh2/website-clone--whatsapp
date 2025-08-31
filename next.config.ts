f21b6716001827d526613a96dadfac543b9ef3ce
Badalsingh2 <127962379+Badalsingh2@users.noreply.github.com>
2025-09-01 00:59:08 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,8 @@
+import type { NextConfig } from "next";
+import path from "node:path";
+
+const LOADER = path.resolve(__dirname, 'src/visual-edits/component-tagger-loader.js');
+
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -22,3 +27,6 @@ const nextConfig: NextConfig = {
     ],
   }
 };
+
+
+export default nextConfig;
